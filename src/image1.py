19ea0b33aaad92f17fe1dd337fa99d0ba5fcc172
Vincenzo <vincenzo.incutti@gmail.com>
2020-11-13 19:30:04 +0000

    skeleton for joint estimation

--- src/image1.py
+++ src/image1.py
@@ -23,11 +23,20 @@ class image_converter:
         self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw", Image, self.callback1)
         # initialize the bridge between openCV and ROS
         self.bridge = CvBridge()
+
+        # Joint position (for now equal to given trajectories)
         self.robot_joint1_pub = rospy.Publisher("/robot/joint1_position_controller/command", Float64, queue_size=10)
         self.robot_joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
         self.robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
         self.robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
-        # Recond the beginning time
+     
+        # Joint estimation w/ computer vision
+        self.joint1_estimation_pub = rospy.Publisher("/estimation/joint1", Float64, queue_size=10)
+        self.joint2_estimation_pub = rospy.Publisher("/estimation/joint2", Float64, queue_size=10)
+        self.joint3_estimation_pub = rospy.Publisher("/estimation/joint3", Float64, queue_size=10)
+        self.joint4_estimation_pub = rospy.Publisher("/estimation/joint4", Float64, queue_size=10)
+
+        # Record the beginning time
         self.start_time = rospy.get_time()
 
     def detect_red(self, image):
@@ -73,36 +82,98 @@ class image_converter:
         return 3 / np.sqrt(dist)
 
 
+    ################### TRAJECTORIES ###################
+
+    def trajectory_joint2(self, image):
+        current_time = rospy.get_time() - self.start_time
+        joint2 = (np.pi/2)*np.sin((np.pi/15)*current_time)
+        return joint2
+
+    def trajectory_joint3(self, image):
+        current_time = rospy.get_time() - self.start_time
+        joint3 = (np.pi/2)*np.sin((np.pi/18)*current_time)
+        return joint3
+
+    def trajectory_joint4(self, image):
+        current_time = rospy.get_time() - self.start_time
+        joint4 = (np.pi/2)*np.sin((np.pi/20)*current_time)
+        return joint4
+
+
+    ################### JOINT ESTIMATION W/ COMPUTER VISION ###################
+    # TODO: 
+    #   - remove orange from image -> this ensures joint spheres can be detected
+
+
+    def estimate_joint1(self, image):
+        pass
+
+
+    def estimate_joint2(self, image):
+        pass
+
+
+    def estimate_joint3(self, image):
+        pass
+
+
+    def estimate_joint4(self, image):
+        pass
+
+
+
+
     # Recieve data from camera 1, process it, and publish
     def callback1(self, data):
-        # Recieve the image
+        # Receive the image
         try:
-            self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
-        current_time = rospy.get_time() - self.start_time
-        joint2d = (np.pi/2)*np.sin((np.pi/15)*current_time)
-        joint3d = (np.pi/2)*np.sin((np.pi/18)*current_time)
-        joint4d = (np.pi/2)*np.sin((np.pi/20)*current_time)
-        joint2=Float64()
-        joint2.data = joint2d
-        joint3=Float64()
-        joint3.data = joint3d
-        joint4=Float64()
-        joint4.data = joint4d
-        self.robot_joint2_pub.publish(joint2)
-        self.robot_joint3_pub.publish(joint3)
-        self.robot_joint4_pub.publish(joint4)
 
 
-        # Uncomment if you want to save the image
-        # cv2.imwrite('image_copy.png', cv_image)
+        # joints given trajectories
+    #   self.joint1 = Float64()
+    #   self.joint1.data = self.trajectory_joint1(cv_image)
+        self.joint2 = Float64()
+        self.joint2.data = self.trajectory_joint2(cv_image1)
+        self.joint3 = Float64()
+        self.joint3.data = self.trajectory_joint3(cv_image1)
+        self.joint4 = Float64()
+        self.joint4.data = self.trajectory_joint4(cv_image1)
 
-        im1 = cv2.imshow('window1', self.cv_image1)
+        # joints estimations w/ computer vision
+        #self.joint1_estimation = Float64()
+        #self.joint1_estimation.data = self.estimate_joint1(cv_image1)  
+        #self.joint2_estimation = Float64()
+        #self.joint2_estimation.data = self.estimate_joint2(cv_image1)
+        #self.joint3_estimation = Float64()
+        #self.joint3_estimation.data = self.estimate_joint3(cv_image1)
+        #self.joint4_estimation = Float64()
+        #self.joint4_estimation.data = self.estimate_joint4(cv_image1)       
+
+
+
+
+        im1 = cv2.imshow('window1', cv_image1)
         cv2.waitKey(1)
         # Publish the results
         try:
-            self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
+            self.image_pub1.publish(self.bridge.cv2_to_imgmsg(cv_image1, "bgr8"))
+
+            # Move joints by following given trajectories
+            #self.robot_joint1_pub.publish(self.joint1)
+            self.robot_joint2_pub.publish(self.joint2)
+            self.robot_joint3_pub.publish(self.joint3)
+            self.robot_joint4_pub.publish(self.joint4)
+
+            # Publish joint estimation w/ computer vision 
+            #self.joint1_estimation_pub.publish(self.joint1_estimation)
+            #self.joint2_estimation_pub.publish(self.joint2_estimation)
+            #self.joint3_estimation_pub.publish(self.joint3_estimation)
+            #self.joint4_estimation_pub.publish(self.joint4_estimation)      
+
+
         except CvBridgeError as e:
             print(e)
 
